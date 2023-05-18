35e4cf7655cb1c52984eb5b5537fb20c569b4ba9
saad <saadansari74002@gmail.com>
2023-05-18 14:28:19 +0530

    add some more things clear

--- style.js
+++ style.js
@@ -1,13 +1,25 @@
 
 
-const squares = 
-document.querySelectorAll('.emoji')
+const squares = document.querySelectorAll('.emoji')
 
-const timesClicked = {'rock': 0, 'paper': 0, 'scissors':0}
+const timesClicked = {'stone': 0, 'paper': 0, 'scissors':0}
 squares.forEach(square => {
     square.onclick = () => {
         timesClicked[square.value] += 1
-        square.innerText = timesClicked[square.value]
+        square.innerText = timesClicked['']
         // console.log(square.value)
     }
 })
+
+function clearScore() {
+    timesClicked.stone = 0
+    timesClicked.paper = 0
+    timesClicked.scissors = 0
+    squares.forEach(square => {
+        square.innerText = '0'
+    }) 
+}
+
+const clearGamebtn = document.getElementById('stop-emoji')
+clearGamebtn.onclick = () => clearScore()
+
